2f75c8c94632dc384108218d31dfff93a35677f5
Almaviva Developer <almavivadeveloper@macbook-pro-di-aleperna.local>
2018-10-19 10:22:35 +0200

    Initial Commit

---
+++ ChatTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatTableView
+//
+//  Created by Alessandro Perna on 19/10/2018.
+//  Copyright © 2018 Alessandro Perna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
